2ed19975a8d11dd883bbae24fe98cf09a565e153
minkwan Park <110644082+min-rhks09@users.noreply.github.com>
2023-08-18 18:02:46 +0900

    feat: tailwindcss 적용

--- portpolio/app/page.js
+++ portpolio/app/page.js
@@ -1,9 +1,12 @@
 import Layout from "@/components/Layout/page"
+import "../styles/global.css"
 
 export default function Home(){
   return(
     <Layout>
-      <h1>HOME!!!!!</h1>
+       <h1 className="text-3xl font-bold underline">
+        Home!
+      </h1>
     </Layout>
   )
 };
